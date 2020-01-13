89bc731e5b88d1defce5c6ee1b89af35cc86dd53
gesab001 <hpminigesab001@gmail.com>
2020-01-14 03:33:27 +1300

    added back buttons and setTitle to change app bar name

--- app/src/main/java/com/giovannisaberon/simplehymn/FullscreenActivity.java
+++ app/src/main/java/com/giovannisaberon/simplehymn/FullscreenActivity.java
@@ -1,6 +1,7 @@
 package com.giovannisaberon.simplehymn;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.ActionBar;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -115,6 +117,14 @@ public class FullscreenActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_fullscreen);
 
+        Button backButton = (Button) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+               startActivity(intent);
+            }
+        });
         hymnJson = new HymnJson(this);
         try {
             hashMapHymns = hymnJson.convertToHashmap("hymns.json");
@@ -127,6 +137,7 @@ public class FullscreenActivity extends AppCompatActivity{
         pref = getApplicationContext().getSharedPreferences("MyPref", 0);
         final int number = pref.getInt("selectedHymnNumber", 0);
         final String title = pref.getString("selectedHymnTitle", null);
+        setTitle(Integer.toString(number) + " - " + title);
         textView = (TextView) mContentView;
         textView.setText(Integer.toString(number) + "\n" + title);
         final ArrayList<List> versesList = hashMapHymns.get(Integer.toString(number));
@@ -160,7 +171,7 @@ public class FullscreenActivity extends AppCompatActivity{
             }
             public void onSwipeRight() {
                 verseCount = verseCount - 1;
-                if (verseCount>versesListWithRefrains.size()){
+                if (verseCount<0){
                     verseCount = 0;
                 }
                 Toast.makeText(FullscreenActivity.this, Integer.toString(verseCount), Toast.LENGTH_SHORT).show();
@@ -186,7 +197,7 @@ public class FullscreenActivity extends AppCompatActivity{
             }
             public void onSwipeLeft() {
                 verseCount = verseCount + 1;
-                if (verseCount>versesListWithRefrains.size()){
+                if (verseCount==versesListWithRefrains.size()-1){
                     verseCount = versesListWithRefrains.size()-1;
                 }
                 Toast.makeText(FullscreenActivity.this, Integer.toString(verseCount), Toast.LENGTH_SHORT).show();
@@ -227,7 +238,7 @@ public class FullscreenActivity extends AppCompatActivity{
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.back_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     public List<String> getNext(List<String> uid) {
